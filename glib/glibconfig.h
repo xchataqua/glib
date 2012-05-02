d99ad6b24de584442e99957d6011a33366a5e197
Jeong YunWon <jeong@youknowone.org>
2012-05-03 03:21:22 +0900

    fix misconfigurated constant

--- glib/glibconfig.h
+++ glib/glibconfig.h
@@ -49,8 +49,8 @@ typedef unsigned int guint32;
 #define G_GUINT32_FORMAT "u"
 #define G_HAVE_GINT64 1          /* deprecated, always true */
 
-typedef signed long gint64;
-typedef unsigned long guint64;
+typedef signed long long gint64;
+typedef unsigned long long guint64;
 
 #define G_GINT64_CONSTANT(val)	(val##L)
 #define G_GUINT64_CONSTANT(val)	(val##UL)
